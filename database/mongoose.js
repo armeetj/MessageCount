cd1ccbc81a961f117bb0b59c057b732edf54ac1b
xarmeetx <xarmeetx@gmail.com>
2019-10-12 01:02:47 -0700

    updated dependencies

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,2 +1,17 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
+
+
+module.exports = 
+{
+    init:() =>
+    {
+        useNewUrlParser: true,
+        autoIndex: false,
+        reconnectInterval: 500,
+        reconnectTries: Number.MAX_VALUE,
+        poolSize: 5,
+        connectTimeoutMS; 10000,
+        family: 4;
+    }
+}
